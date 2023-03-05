d90001a16b53e63193f9a03ca0716da2a7fcdc4a
Eugene <eugeneh101@DESKTOP-6EJMNGA>
2023-03-05 11:59:43 -0500

    replaced boto3's redshift-data with redshift-connnector

--- source/load_s3_files_from_dynamodb_stream_to_redshift_lambda/handler.py
+++ source/load_s3_files_from_dynamodb_stream_to_redshift_lambda/handler.py
@@ -1,7 +1,7 @@
 import os
-import time
 
 import boto3
+import redshift_connector
 
 AWS_REGION = os.environ["AWSREGION"]
 
@@ -12,11 +12,11 @@ S3_BUCKET_FOR_DYNAMODB_STREAM_TO_REDSHIFT = os.environ[
 UNPROCESSED_DYNAMODB_STREAM_FOLDER = os.environ["UNPROCESSED_DYNAMODB_STREAM_FOLDER"]
 PROCESSED_DYNAMODB_STREAM_FOLDER = os.environ["PROCESSED_DYNAMODB_STREAM_FOLDER"]
 
-redshift_data_client = boto3.client("redshift-data")
 # aws_redshift.CfnCluster(...).attr_id (for cluster name) is broken, so using endpoint address instead
-REDSHIFT_CLUSTER_NAME = os.environ["REDSHIFT_ENDPOINT_ADDRESS"].split(".")[0]
-REDSHIFT_ROLE_ARN = os.environ["REDSHIFT_ROLE_ARN"]
+REDSHIFT_HOST = os.environ["REDSHIFT_ENDPOINT_ADDRESS"].split(":")[0]
 REDSHIFT_USER = os.environ["REDSHIFT_USER"]
+REDSHIFT_PASSWORD = os.environ["REDSHIFT_PASSWORD"]
+REDSHIFT_ROLE_ARN = os.environ["REDSHIFT_ROLE_ARN"]
 REDSHIFT_DATABASE_NAME = os.environ["REDSHIFT_DATABASE_NAME"]
 REDSHIFT_SCHEMA_NAME_FOR_DYNAMODB_CDC = os.environ[
     "REDSHIFT_SCHEMA_NAME_FOR_DYNAMODB_CDC"
@@ -24,30 +24,6 @@ REDSHIFT_SCHEMA_NAME_FOR_DYNAMODB_CDC = os.environ[
 REDSHIFT_TABLE_NAME_FOR_DYNAMODB_CDC = os.environ[
     "REDSHIFT_TABLE_NAME_FOR_DYNAMODB_CDC"
 ]
-
-
-def execute_sql_statement(sql_statement: str) -> None:
-    response = redshift_data_client.execute_statement(
-        ClusterIdentifier=REDSHIFT_CLUSTER_NAME,
-        Database=REDSHIFT_DATABASE_NAME,
-        DbUser=REDSHIFT_USER,
-        Sql=sql_statement,
-    )
-    time.sleep(1)
-    while True:
-        response = redshift_data_client.describe_statement(Id=response["Id"])
-        status = response["Status"]
-        if status == "FINISHED":
-            print(f"Finished executing the following SQL statement: {sql_statement}")
-            return
-        elif status in ["SUBMITTED", "PICKED", "STARTED"]:
-            time.sleep(1)
-        elif status == "FAILED":
-            print(response)
-            raise  ### figure out useful message in exception
-        else:
-            print(response)
-            raise  ### figure out useful message in exception
 
 
 def move_s3_file(s3_bucket: str, old_s3_filename: str, new_s3_filename) -> None:
@@ -83,7 +59,17 @@ def lambda_handler(event, context) -> None:
                     iam_role '{REDSHIFT_ROLE_ARN}'
                     format as json 'auto';
                 """
-                execute_sql_statement(sql_statement=sql_statement)
+                conn = redshift_connector.connect(
+                    host=REDSHIFT_HOST,
+                    database=REDSHIFT_DATABASE_NAME,
+                    user=REDSHIFT_USER,
+                    password=REDSHIFT_PASSWORD,
+                )
+                cursor = conn.cursor()
+                with conn, cursor:
+                    cursor.execute(sql_statement)
+                    conn.commit()
+                    print(f"Finished executing the following SQL statement: {sql_statement}")
                 move_s3_file(
                     s3_bucket=S3_BUCKET_FOR_DYNAMODB_STREAM_TO_REDSHIFT,
                     old_s3_filename=s3_file,
